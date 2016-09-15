1bfceda47de8ca8e8ad5f8b4a99bf0abb01c352b
Jackson Taylor <closprieur@gmail.com>
2016-09-15 18:29:13 +1000

    Lock final version with working code
    Adjusts the code to dynamically add labels and break to a new stack view once line reaches a certain length, calculated proportionately to screen size.

--- useage-layout-test3/ViewController.swift
+++ useage-layout-test3/ViewController.swift
@@ -10,23 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Create main stack view container with centre x and y constraints in storyboard
+    
+    
+    @IBOutlet weak var topStackView: UIStackView!
+    
+    @IBOutlet weak var bottomStackView: UIStackView!
+    
+    
+    
+    // Create a horizontal stackview
+    //let horizontalStackView = UIStackView()
+    
+    
     // Set up a UILabel to style and add programmatically later
-    let outputLabel = UILabel()
+    
     
     // For convenience the words are added as separate indicies; this will be a sentence that will be parsed according to the occurrence of a space in future
     let outputArray = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
     
-    @IBOutlet weak var topStackView: UIStackView!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    
-        outputLabel.font = UIFont(name: "Futura-Medium", size: 24.0)
-        outputLabel.text = "Hello world"
-        topStackView.addArrangedSubview(outputLabel)
-    
+
+        var lineWidth: CGFloat = 0
+        
+        // Set maximum width allowed until you force label into a new stack view
+        // Calculate device size
+        
+        let bounds = UIScreen.mainScreen().bounds
+        let width = bounds.size.width
+        
+        let maximumAllowedLineWidth: CGFloat = width * 0.8 // Might have to reset this to recalculate upon rotation
+        
+        print("Calculated Maximum Line Width is \(maximumAllowedLineWidth)")
+        
+        for i in 0..<outputArray.count {
+        
+            let outputLabel = UILabel()
+            
+            outputLabel.font = UIFont(name: "Futura-Medium", size: 24.0)
+            
+            outputLabel.text = outputArray[i]
+            
+            let labelWidth = outputLabel.intrinsicContentSize().width
+            
+            print(labelWidth)
+            
+            if lineWidth + labelWidth < maximumAllowedLineWidth {
+            
+                topStackView.addArrangedSubview(outputLabel)
+            
+            } else {
+            
+                bottomStackView.addArrangedSubview(outputLabel)
+            
+            }
+            
+            lineWidth += labelWidth
+        
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
