ed656b709c633b0464021bff67a8556797ed3684
Jackson Taylor <closprieur@gmail.com>
2016-09-15 14:40:30 +1000

    Add styling to label and add to top stack view programmatically

--- useage-layout-test3/ViewController.swift
+++ useage-layout-test3/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Set up a UILabel to style and add programmatically later
+    let outputLabel = UILabel()
+    
+    // For convenience the words are added as separate indicies; this will be a sentence that will be parsed according to the occurrence of a space in future
+    let outputArray = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+    
+    @IBOutlet weak var topStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        outputLabel.font = UIFont(name: "Futura-Medium", size: 24.0)
+        outputLabel.text = "Hello world"
+        topStackView.addArrangedSubview(outputLabel)
+    
     }
 
     override func didReceiveMemoryWarning() {
