4cb5bc81771f8ab566d9afe4b6ea9583fc75b14a
Jackson Taylor <closprieur@gmail.com>
2016-09-15 13:08:34 +1000

    Initial Commit

---
+++ useage-layout-test3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  useage-layout-test3
+//
+//  Created by Jackson Taylor on 15/09/2016.
+//  Copyright © 2016 Jackson Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
